a12392d464b14e649910505f00a9d8ff2e246ba3
Raul Zarza <raulzarza.dev@gmail.com>
2020-07-25 13:48:18 -0600

    Se cancelaron las rutas protegidas. para falcilitar el desarrollo y por que no funcionan XD

--- src/routes/entrenos.routes.js
+++ src/routes/entrenos.routes.js
@@ -5,12 +5,12 @@ const isAuthenticated = require("../authenticatons/jwt.authentication");
 const { getEntrenos, createEntreno, getEntreno, updateEntreno, deleteEntreno } = require('../controllers/entreno.controllers')
 
 router.route('/')
-    .get(isAuthenticated, getEntrenos)
-    .post(isAuthenticated, createEntreno)
+    .get(/* isAuthenticated, */ getEntrenos)
+    .post(/* isAuthenticated,  */createEntreno)
 
 router.route('/:id')
-    .get(isAuthenticated, getEntreno)
-    .delete(isAuthenticated, deleteEntreno)
+    .get(/* isAuthenticated, */ getEntreno)
+    .delete(/* isAuthenticated, */ deleteEntreno)
 
 //.put('/:id', updateEntreno)
 
